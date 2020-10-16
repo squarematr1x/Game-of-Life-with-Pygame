bfc8167dc292453a0822d8658559e93d86f4f836
squarematr1x <sasu.pirttipera@protonmail.com>
2020-10-16 14:04:14 +0300

    Replaced arrays with numpy arrays

--- gol_pygame/GOL.py
+++ gol_pygame/GOL.py
@@ -1,22 +1,24 @@
 import random
+import numpy as np
 
 class GameOfLife:
 
     def __init__(self, grid_width, grid_height):
         self.grid_width = grid_width
         self.grid_height = grid_height
-        self.grid = [[0 for x in range(grid_width)] for y in range(grid_height)]
+        self.grid = np.array([[0 for x in range(grid_width)] for y in range(grid_height)])
         self.pct_of_living = 0
         self.count = 0
 
     # Initializes the grid with random cells
     def initialize_grid(self):
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
-                self.grid[i][j] = random.choice([0] * 7 + [1] * 3)
+       self.grid = np.random.choice([0, 1], (self.grid_height, self.grid_width), [.8, .2])
 
+    def empty_grid(self):
+       self.grid = np.zeros((self.grid_height, self.grid_width))
+       
     def update_grid(self):
-        next_grid = [[0 for x in range(self.grid_width)] for y in range(self.grid_height)]
+        next_grid = np.array([[0 for x in range(self.grid_width)] for y in range(self.grid_height)])
         alive = 0
         count = 0
 
@@ -35,15 +37,10 @@ class GameOfLife:
                         alive += 1
                     else:
                         next_grid[i][j] = 0
+
         self.count = alive
         self.pct_of_living = alive / count
-
         self.grid = next_grid
-
-    def empty_grid(self):
-        for i in range(len(self.grid)):
-            for j in range(len(self.grid[0])):
-                self.grid[i][j] = 0
 
     # Count the number of neighbours
     def neighbours(self, row, col):
